78a40bd1e46f01d97113684086a76e215593a5f0
Anand5329 <46213999+Anand5329@users.noreply.github.com>
2019-06-02 18:53:55 +0530

    broke createComboBoxItems(formerly createMenuItems) into sub-methods

--- AddScheduleWindowController.java
+++ AddScheduleWindowController.java
@@ -32,7 +32,7 @@ public class AddScheduleWindowController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        createMenuItems(schedule);
+        createComboBoxItems();
     }
 
     @FXML
@@ -57,7 +57,10 @@ public class AddScheduleWindowController implements Initializable {
     private Label descriptionLabel;
 
     @FXML
-    private ComboBox slotChooser;
+    private ComboBox slotChooserStart;
+
+    @FXML
+    private ComboBox slotChooserEnd;
 
     @FXML
     private TextField nameTextField;
@@ -91,7 +94,7 @@ public class AddScheduleWindowController implements Initializable {
     }
 
     @FXML
-    public void slotChooser(ActionEvent event)
+    public void slotChooserStart(ActionEvent event)
     {
         MenuItem newSlot=(MenuItem)event.getSource();
         slot=new Slot(newSlot.getText());
@@ -102,62 +105,78 @@ public class AddScheduleWindowController implements Initializable {
     public void cancel()
     {}
 
-    public void createMenuItems(Schedule schedule)
+    public LinkedList<String> generateAllSlots()
     {
-         LinkedList<Slot> slots=new LinkedList<>();
-         try
-         {
-             slots=schedule.getSlots();
-         }
-         catch(NullPointerException e)
-         {
-             System.err.println("Null pointer exception, probably because File was not found");
-         }
-         LinkedList<String> occupiedSlots=new LinkedList<>();
-         Iterator itr=slots.iterator();
-         while(itr.hasNext())
-         {
-             occupiedSlots.add(((Slot)itr.next()).displaySlot());
-         }
-         LinkedList<String> availableSlots=new LinkedList<>();
-         double ctr=15;
-         while(ctr<21)
-         {
-             double minutes[]={0.00,0.25,0.50,0.75};
-             for(int i=0;i<3;i++)
-             {
+        LinkedList<String> availableSlots=new LinkedList<>();
+        double ctr=15;
+        while(ctr<21)
+        {
+            double minutes[]={0.00,0.25,0.50,0.75};
+            for(int i=0;i<3;i++)
+            {
 
-                 availableSlots.add((ctr+minutes[i])+" - "+(ctr+minutes[i+1]));
-             }
-             availableSlots.add((ctr+minutes[3])+" - "+(++ctr+minutes[0]));
-         }
+                availableSlots.add((ctr+minutes[i])+" - "+(ctr+minutes[i+1]));
+            }
+            availableSlots.add((ctr+minutes[3])+" - "+(++ctr+minutes[0]));
+        }
+        return availableSlots;
+    }
 
-         for(int i=0;i<occupiedSlots.size();i++)
-         {
-             for(int j=0;j<availableSlots.size();j++)
-             {
-                 if(occupiedSlots.get(i).equals(availableSlots.get(j)))
-                 {
-                     availableSlots.remove(j);
-                     break;
-                 }
-             }
-         }
+    public LinkedList<String> generateOccupiedSlots()
+    {
+        LinkedList<Slot> slots=new LinkedList<>();
+        try
+        {
+            slots=schedule.getSlots();
+        }
+        catch(NullPointerException e)
+        {
+            System.err.println("Null pointer exception, probably because File was not found");
+        }
+        LinkedList<String> occupiedSlots=new LinkedList<>();
+        Iterator itr=slots.iterator();
+        while(itr.hasNext())
+        {
+            occupiedSlots.add(((Slot)itr.next()).displaySlot());
+        }
+        return occupiedSlots;
+    }
+
+    public LinkedList<String> generateAvailableSlots(LinkedList<String> availableSlots,LinkedList<String> occupiedSlots)
+    {
+        for(int i=0;i<occupiedSlots.size();i++)
+        {
+            for(int j=0;j<availableSlots.size();j++)
+            {
+                if(occupiedSlots.get(i).equals(availableSlots.get(j)))
+                {
+                    availableSlots.remove(j);
+                    break;
+                }
+            }
+        }
+        return availableSlots;
+    }
+
+    public void createComboBoxItems()
+    {
+         LinkedList<String> occupiedSlots=generateOccupiedSlots();
+
+         LinkedList<String> availableSlots=generateAllSlots();
+
+         availableSlots=generateAvailableSlots(availableSlots,occupiedSlots);
+
          ObservableList<String> data=FXCollections.observableList(availableSlots);
+
          try {
-             //for (int i = 0; i < availableSlots.size(); i++) {
-//             slotChooser.getItems().clear();
-//             slotChooser.getItems().addAll("Slot1","Slot2");
-//             slotChooser=new ComboBox();
-             slotChooser.setPromptText("Time slots");
-             slotChooser.setItems(data);
+             slotChooserStart.setPromptText("Start Time");
+             slotChooserEnd.setPromptText("End Time");
+             slotChooserStart.setItems(data);
              }
 
          catch(NullPointerException e)
          {
              System.err.println("Null pointer exception, probably because File was not found");
          }
-         //slotChooser.show();
-
     }
 }
